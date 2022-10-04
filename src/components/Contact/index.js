99b733d5a724bec1ea17b4716c992d64dc11c67f
andcama <andcamachoa@gmail.com>
2022-10-04 09:03:22 -0600

    second commit

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { useRef } from 'react'
 import emailjs from '@emailjs/browser'
 import AnimatedLetters from '../AnimatedLetters'
@@ -83,24 +82,6 @@ const Contact = () => {
               </ul>
             </form>
           </div>
-        </div>
-        <div className="info-map">
-         Andres Camacho,
-          <br />
-          Costa Rica,
-          <br />
-          San Antonio,Desamparados<br />
-          San Jose <br />
-          <br />
-          <span>andcamachoa@gmail.com</span>
-        </div>
-        <div className="map-wrap">
-          <MapContainer center={[9.900075,-84.055027]} zoom={13}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <Marker position={[9.900075,-84.055027]}>
-              <Popup>Sloba lives here, come over for a cup of coffee :)</Popup>
-            </Marker>
-          </MapContainer>
         </div>
       </div>
       <Loader type="pacman" />
